d798e62d8b50c859159b8ea42f994bfa9650dff0
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-07 13:08:58 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,13 @@
  *
  **/
 
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
-	char c='a';
+	char c;
 	int i=0;
-	while(i<10){
-		while(c<='z'){
+	while(i < 10){
+		c='a';
+		while(c <= 'z'){
 			_putchar(c);
 			c++;
 		}
