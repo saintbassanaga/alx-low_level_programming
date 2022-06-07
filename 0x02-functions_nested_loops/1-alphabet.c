1cf41e39eb5be6de8f0a1c5eaa1f8835d93c79d7
saintbassanaga <saintbassanaga01@gmail.com>
2022-06-07 03:32:05 -0700

    Creating 1-Alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ * Return: Always 0.
+ **/
+
+int main(void)
+{
+	char c='a';
+	while (c<z)
+	{
+		_putch(c);
+		c++;
+	}
+	_putchar("\n");
+}
