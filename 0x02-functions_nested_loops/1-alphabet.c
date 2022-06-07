77eca191f48b919d833f88015b2628e9366c5e1e
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-07 12:04:46 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,16 @@
  * Return: Always 0.
  **/
 
-int main(void)
+void print_alphabet(void)
 {
-	char c='a';
-	while (c<'z')
+	char c = 'a';
+
+	while (c <= 'z')
 	{
-		_putchar(&c);
+		_putchar(c);
 		c++;
+
 	}
-	_putchar("\n");
-	
-	return 0;
+	_putchar('\n');
+
 }
