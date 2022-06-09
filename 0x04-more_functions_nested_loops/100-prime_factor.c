2b0b0a61072a0da59ca16eaa8f591c4d3e995290
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-09 23:03:29 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,16 +11,14 @@ int main(void)
 unsigned int long n = 612852475143;
 unsigned int long a = (int) sqrt(n);
 
-while (1)
-{
-
-if (n % a == 0)
-{
-printf("%lu \n", n / a);
-break;
-}
-a--;
-}
-
-return (0);
+	while (1)
+	{
+		if (n % a == 0)
+		{
+			printf("%lu \n", n / a);
+			break;
+		}
+		a--;
+	}
+	return (0);
 }
