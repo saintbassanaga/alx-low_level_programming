d5db9326bf7c3c8cee8ff577acf199c6e9796307
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-09 13:46:43 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  *print_most_numbers - print all number from 0 to 9 excepting 2 and 4
- *
  *Return: Always 0
  */
 
 void print_most_numbers(void)
 {
 	char n = 0;
+	
 	while (n <= 9)
 	{
 		if (n != 2 && n != 4)
