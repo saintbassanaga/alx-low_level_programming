5b2c89e4fde5476562ea344413a01bf68b3aec15
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-09 14:32:00 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - render the line with n value
- * @n : number of times the character _ should be printed 
- *
- * Return : 0
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
 
 void print_line(int n)
 {
+
 	int i = 0;
 
-	while (i < n &&  n>0)
+	while (i < n && n > 0)
 	{
 		_putchar('_');
 		i++;
 	}
 	_putchar('\n');
+
 }
