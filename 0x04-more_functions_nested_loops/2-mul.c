3c7824d10c98f2c9272739dfb6b8273aff9cb3ae
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-09 12:55:44 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *mul - multiply to number
- *@c - is the first integer
- *@d - is the second integer
+ *@c: is the first integer
+ *@d: is the second integer
  * Return : Always 0
  */
 
 
 int mul(int c, int d)
 {
-return(c * d);
+return(a * b);
 }
