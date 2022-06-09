d96d9ef486d8576d741ae4b3d9195bed2f2b06e6
saintbassanaga <saintbassanaga01@gmail.com>
2022-06-09 05:35:14 -0700

    Creating 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print all number from 0 to 9 excepting 2 and 4
+ *
+ *Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	char c=0;
+	while(c<10)
+	{
+		if(c!=2 || c!=4)
+			_putchar('0' +c);
+		c++;
+	}
+	_putchar('\n');
+}
+
