3367e61e0ce05923cbfb13e2e3193436cd5d4c8f
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-16 05:10:38 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- *
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-{
-	if (s1[i] != s2[i])
+	int i;
+	
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-}
-return (0);
+	return (0);
 }
