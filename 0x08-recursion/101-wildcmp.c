218b2a9ead8cb99214d75a36dd2717588ec0f4c2
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-23 02:20:23 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 
 /**
+ * checkLast - check last char of s2 when s1 ends
+ * @s: string
+ * @i: int
+ * Return: 0 or 1
+ */
+int checkLast(char *s, int i)
+{
+if (s[i] == '*')
+	return (checkLast(s, i + 1));
+else if (s[i] == '\0')
+	return (1);
+
+return (0);
+
+}
+/**
  * checker - helper
  * @s1: string
  * @s2: string
@@ -56,19 +72,4 @@ int wildcmp(char *s1, char *s2)
 return (checker(s1, s2, 0, 0, -1));
 }
 
-/**
- * checkLast - check last char of s2 when s1 ends
- * @s: string
- * @i: int
- * Return: 0 or 1
- */
-int checkLast(char *s, int i)
-{
-if (s[i] == '*')
-	return (checkLast(s, i + 1));
-else if (s[i] == '\0')
-	return (1);
 
-return (0);
-
-}
