23173bb503155f5e16f695a30fdc8a1321bfb5b9
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-23 02:19:01 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,34 +2,6 @@
 #include <stdio.h>
 
 /**
- * wildcmp - check the code for Holberton School students.
- * @s1: string
- * @s2: string
- * Return: Always 0.
- */
-
-int wildcmp(char *s1, char *s2)
-{
-return (checker(s1, s2, 0, 0, -1));
-}
-
-/**
- * checkLast - check last char of s2 when s1 ends
- * @s: string
- * @i: int
- * Return: 0 or 1
- */
-int checkLast(char *s, int i)
-{
-if (s[i] == '*')
-	return (checkLast(s, i + 1));
-else if (s[i] == '\0')
-	return (1);
-
-return (0);
-
-}
-/**
  * checker - helper
  * @s1: string
  * @s2: string
@@ -69,5 +41,34 @@ if (s2[b] != '\0')
 	return (checkLast(s2, b));
 
 return (1);
+
+}
+
+/**
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string
+ * @s2: string
+ * Return: Always 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+return (checker(s1, s2, 0, 0, -1));
+}
+
+/**
+ * checkLast - check last char of s2 when s1 ends
+ * @s: string
+ * @i: int
+ * Return: 0 or 1
+ */
+int checkLast(char *s, int i)
+{
+if (s[i] == '*')
+	return (checkLast(s, i + 1));
+else if (s[i] == '\0')
+	return (1);
+
+return (0);
 
 }
