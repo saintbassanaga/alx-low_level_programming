4e0408583e0b5254fc13890719acb3f680d5d8e8
saintbassanaga <saintbassanaga01@gmail.com>
2022-06-24 06:15:49 -0700

    Updating the function that print his file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main - return file name .
- *@argc : unused .
- *
- *Return : 0 .
+
+/* main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
