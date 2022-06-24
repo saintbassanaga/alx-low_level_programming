621d8ab09f6e1dddd92e2faed7c9c614a7dc31c7
saintbassanaga <saintbassanaga01@gmail.com>
2022-06-24 06:09:49 -0700

    Adding the function that the return his file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
-
-
-int main(int argc __attribute__((unused)) , char* argv[])
+/*
+ * main - return file name .
+ *@argc : unused .
+ *
+ *Return : 0 .
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
