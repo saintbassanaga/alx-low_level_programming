5b52c825bdce76aab51c21bdc93d9ff59dff0cfb
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-30 16:07:34 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,22 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+	unsigned long mul;
+	int i, j;
+	char *n = "Error\n";
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{ 
+		while(n != '\0')
+			_putchar(n);
+		exit(98); 
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{  
+				while(n != '\0')
+					_putchar(n);
+				exit(98); 
+			}
 		}
 
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+	_putchar("%lu\n", mul);
 return (0);
 }
