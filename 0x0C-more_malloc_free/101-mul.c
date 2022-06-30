536e1a1357d5cea0fe4213fe9b19990baa3277f8
Saint Bassanaga <saintbassanaga01@gmail.com>
2022-06-30 18:15:16 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - multiplies two positive numbers
@@ -7,31 +7,23 @@
  * @argv: args
  * Return: int
  */
-int main(int argc, char *argv[])
-{
-	unsigned long mul;
-	int i, j;
-	char *n = "Error\n";
-	if (argc != 3)
-	{ 
-		while(n != '\0')
-			_putchar(n);
-		exit(98); 
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  
-				while(n != '\0')
-					_putchar(n);
-				exit(98); 
-			}
-		}
+int main(int argc, char *argv[]) {
+    unsigned long mul;
+    int i, j;
+    if (argc != 3) {
+        printf("Error\n");
+        exit(98);
+    }
+    for (i = 1; i < argc; i++) {
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (argv[i][j] > 57 || argv[i][j] < 48) {
+                printf("Error\n");
+                exit(98);
+            }
+        }
 
-	}
-	mul = (int*)argv[1] *  (int*)argv[2];
-	_putchar("%lu\n", mul);
-return (0);
+    }
+  mul = atol(argv[1]) * atol(argv[2]);
+    printf("%lu\n", mul);
+    return (0);
 }
