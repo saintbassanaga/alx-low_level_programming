ce37ceff02cd332a3b867cd343b2514c3a81c75b
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-04 15:30:54 -0700

    SIZE 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#include <stdlib.c>
+/**
+ * Hearder file : Macro abbreviation of SIZE
+ *SIZE : 1024
+ *
+ */
+
+#ifndef INC_100_MUL_MAN_H
+#define INC_100_MUL_MAN_H
+
+#define SIZE 1024
+
+
+#endif // 0_OBJECT_LIKE_MACRO_H
