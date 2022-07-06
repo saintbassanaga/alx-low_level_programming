2fb07590aaddca0e0f22e1623e5c8fe5abb513de
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-06 05:30:21 -0700

    Print name file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name
+ * @name : pointer to name
+ * @f : pointer to char function
+ *
+ * Return : Null
+ */
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		*f)(name);
+}
