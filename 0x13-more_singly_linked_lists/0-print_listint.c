b17d93892add2ab6918d68422641a45d5a83aaba
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-19 00:19:08 +0100

    File Update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,7 +12,7 @@
 size_t print_listint(const listint_t *h){
     size_t nnodes =0;
     while (h!=NULL){
-      /*  printf("%d\n", h->n);*/
+      printf("%d\n", h->n);
         h=h->next;
         nnodes++;
     }
