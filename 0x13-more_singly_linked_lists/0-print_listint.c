2b10c97af8c38ec1644cc025cd0e495a01bce39b
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-19 01:14:20 +0100

    File Update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,15 @@
  * You are allowed to use printf
 */
 
-size_t print_listint(const listint_t *h){
-    size_t nnodes =0;
-    while (h!=NULL){
-      printf("%d\n", h->n);
-        h=h->next;
-        nnodes++;
-    }
-    return nnodes;
+size_t print_listint(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
