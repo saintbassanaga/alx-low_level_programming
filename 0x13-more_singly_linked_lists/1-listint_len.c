2032d0a295c842146a2519cbd9347c1f9c5af59e
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-19 14:44:29 +0100

    Creating 1-listint_len.c file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint()- prints all the elements of a listint_t list.
+ * @h : struct initialisation
+ * Return: the number of nodes
+ * You are allowed to use printf
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
