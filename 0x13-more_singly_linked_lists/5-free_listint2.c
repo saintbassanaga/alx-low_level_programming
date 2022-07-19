b5336c2576405a65ea46b90bc003f63c99363cff
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-20 00:04:53 +0100

    updating 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *curr;
 
-	while ((temp = *head) != NULL)
+	if (head != NULL)
 	{
-		temp = NULL;
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
 }
