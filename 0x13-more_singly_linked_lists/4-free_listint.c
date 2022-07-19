5afd66b9d2e39bcca72e605428609f49cebaf80d
saintbassanaga <saintbassanaga01@gmail.com>
2022-07-19 23:36:59 +0100

    Creating 4-free_listint.c file

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint()-free list of integer
+ * @head pointer to the list
+ * Return: 0
+*/
+
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+
+    while ((temp = head) != NULL)
+    {
+        head = head->next;
+        free(temp);
+    }
+}
